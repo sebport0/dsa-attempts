ab2ed93a9b224bf0e412d67dc2faeef883af6c00
sebport0 <8371137+sebport0@users.noreply.github.com>
2025-01-29 11:40:04 -0300

    add leetcode's 3063 solutions using hash map and with hash map in one iteraton

--- problems/leetcode/3063_linked_list_frequency/3063_linked_list_frequency.cpp
+++ problems/leetcode/3063_linked_list_frequency/3063_linked_list_frequency.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <iostream>
 #include <sys/types.h>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -40,6 +41,46 @@ public:
     }
     return result_head->next;
   }
+
+  // V2: use a hash map instead of an array to
+  // count the frequencies.
+  ListNode *frequenciesOfElementsV2(ListNode *head) {
+    // freqs<node values, appeareances count>
+    unordered_map<int, int> freqs = {};
+    while (head != nullptr) {
+      freqs[head->val] += 1;
+      head = head->next;
+    }
+    ListNode *result = new ListNode();
+    ListNode *curr = result;
+    for (pair<const int, int> &item : freqs) {
+      curr->next = new ListNode(item.second);
+      curr = curr->next;
+    }
+    return result->next;
+  }
+
+  // V3: use a hash map but instead of doing a double pass:
+  //   1. Original linked list.
+  //   2. Hash map with frequencies.
+  // Do it in one pass of the original linked list.
+  ListNode *frequenciesOfElementsV3(ListNode *head) {
+    unordered_map<int, ListNode *> freqs;
+    ListNode *prev = nullptr;
+    ListNode *curr = head;
+    while (curr != nullptr) {
+      // If curr ListNode doesn't exists in hash map.
+      if (freqs.find(curr->val) == freqs.end()) {
+        ListNode *newFreq = new ListNode(1, prev);
+        freqs[curr->val] = newFreq;
+        prev = newFreq;
+      } else {
+        freqs[curr->val]->val += 1;
+      }
+      curr = curr->next;
+    }
+    return prev;
+  }
 };
 
 ListNode *makeListNode(vector<int> v) {
@@ -66,11 +107,32 @@ void printListNode(ListNode *list) {
 
 int main() {
   Solution s = Solution();
-  // vector<int> v1 = {1, 1, 2, 1, 2, 3};
-  // ListNode *l1 = s.frequenciesOfElements(makeListNode(v1));
-  // printListNode(l1);
+  cout << "== Using array == " << endl;
+  vector<int> v1 = {1, 1, 2, 1, 2, 3};
+  ListNode *l1 = s.frequenciesOfElements(makeListNode(v1));
+  printListNode(l1);
   vector<int> v2 = {3, 6, 3, 6, 8, 6};
   ListNode *l2 = s.frequenciesOfElements(makeListNode(v2));
   printListNode(l2);
+  cout << endl;
+
+  cout << "== Using unordered hash map == " << endl;
+  vector<int> v = {1, 1, 2, 1, 2, 3};
+  ListNode *l3 = s.frequenciesOfElementsV2(makeListNode(v));
+  printListNode(l3);
+  v = {3, 6, 3, 6, 8, 6};
+  ListNode *l4 = s.frequenciesOfElements(makeListNode(v));
+  printListNode(l4);
+  cout << endl;
+
+  cout << "== Using unordered hash map with one iterarion == " << endl;
+  v = {1, 1, 2, 1, 2, 3};
+  ListNode *l5 = s.frequenciesOfElementsV3(makeListNode(v));
+  printListNode(l5);
+  v = {3, 6, 3, 6, 8, 6};
+  ListNode *l6 = s.frequenciesOfElements(makeListNode(v));
+  printListNode(l6);
+  cout << endl;
+
   return 0;
 }
