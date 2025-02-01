e0d6386656f5c0377135b8746c6d4e633844651c
sebport0 <8371137+sebport0@users.noreply.github.com>
2025-02-01 18:11:42 -0300

    add bash script to write new entry to README

---
+++ add_problem_to_readme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+name=$1
+lang=$2
+source=$3
+
+if [ "${source,,}" == "leetcode" ]; then
+  link=$(echo "$name" | sed 's/^[0-9]*\. //')
+  link=${link// /-}
+  link="https://leetcode.com/problems/$(echo "$link" | tr "[:upper:]" "[:lower:]")"
+  code_path=${name//". "/_}
+  code_path=$(echo "$code_path" | tr " " "_" | tr "[:upper:]" "[:lower:]")
+  code_path="/problems/leetcode/$code_path"
+fi
+echo "| [$name]($link) | [$lang]($code_path) | $source |" >> README.md
