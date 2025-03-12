f83033a069a48fa191dc2e31db9eb805bfab8819
sebport0 <8371137+sebport0@users.noreply.github.com>
2025-03-12 00:11:26 -0300

    add leetcode's 1570

--- leetcode/1570_dot_product_of_two_sparse_vectors/1570_dot_product_of_two_sparse_vectors.cpp
+++ leetcode/1570_dot_product_of_two_sparse_vectors/1570_dot_product_of_two_sparse_vectors.cpp
@@ -1,23 +1,45 @@
 // https://leetcode.com/problems/dot-product-of-two-sparse-vectors
 #include <algorithm>
-#include <array>
 #include <cassert>
-#include <iostream>
-#include <map>
-#include <set>
-#include <string>
 #include <sys/types.h>
 #include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
-class Solution {
+class HashSparseVector {
+
 public:
+  unordered_map<int, int> non_zeros = {};
+  HashSparseVector(vector<int> &nums) {
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] != 0) {
+        non_zeros[i] = nums[i];
+      }
+    }
+  }
+  int dotProduct(HashSparseVector &vec) {
+    int result = 0;
+    for (auto i = non_zeros.begin(); i != non_zeros.end(); ++i) {
+      if (vec.non_zeros.count(i->first) != 0) {
+        result += i->second * vec.non_zeros[i->first];
+      }
+    }
+    return result;
+  }
 };
 
-int main() {Solution s = Solution();
-  
-  return 0;
-}
+class SlowSparseVector {
+public:
+  vector<int> ns;
+  SlowSparseVector(vector<int> &nums) { ns = nums; }
+  int dotProduct(SlowSparseVector &vec) {
+    int result = 0;
+    for (int i = 0; i < vec.ns.size(); i++) {
+      result += ns[i] * vec.ns[i];
+    }
+    return result;
+  }
+};
+
+int main() { return 0; }
--- leetcode/1570_dot_product_of_two_sparse_vectors/log.md
+++ leetcode/1570_dot_product_of_two_sparse_vectors/log.md
@@ -2,8 +2,29 @@
 
 [Link](https://leetcode.com/problems/dot-product-of-two-sparse-vectors)
 
+## Linear
+
+"Linear" because this linear solution doesn't make use of the fact that the
+vectors are sparse, so there are a lot of multiplications that could be
+avoided.
+
 - Complexity
 
-  - Time: $$
+  - Space: $O(1)$ because the sparse vector class holds the reference to
+    another vector.
 
-  - Space: $$
+  - Time: $O(n)$, the dot product needs to iterate over the vectors size
+    to compute the final result.
+
+## Hash
+
+Using a hash map is possible to make the most of the sparse condition of
+the vector. We'll store only the indexes, as keys, and the elements, as values,
+of the vector that are not 0. To calculate the dot product, we only need to
+check if the same key is in the other sparse vector.
+
+- Complexity
+  - Space: $O(n)$ to store the hash map(only the non zero elements) and $O(1)$
+    the dot product calculation.
+  - Time: $O(n)$ for calculating the dot product and the same for iterating over
+    the given sparse vector on construction.
